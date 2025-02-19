034ec9662a9ea6f230b4d7f7d9a98adb53d9d678
shalooverma1 <99644916+shalooverma1@users.noreply.github.com>
2025-02-19 18:10:23 +0530

    Update utils.py

--- incident_management/incidents/utils.py
+++ incident_management/incidents/utils.py
@@ -31,16 +31,13 @@ def fetch_and_store_incidents(api_url):
             }
     
     except requests.exceptions.HTTPError as http_err:
-        # Specific handling for HTTP errors
         logger.error(f"HTTP error occurred: {http_err}")
         return {"error": "HTTP error occurred", "details": str(http_err)}
     
     except requests.exceptions.RequestException as req_err:
-        # Catch other request-related errors
         logger.error(f"Request error occurred: {req_err}")
         return {"error": "Request error occurred", "details": str(req_err)}
     
     except Exception as e:
-        # Catch any other unexpected errors
         logger.error(f"An unexpected error occurred: {e}")
         return {"error": "An unexpected error occurred", "details": str(e)}
